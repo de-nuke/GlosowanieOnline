65865658be15fb8c3530745dec4b46f085ff6a2c
Charles <makosakarol@gmail.com>
2018-01-16 21:57:47 +0100

    No idea mate

--- webfront/src/onlinevotingclient.py
+++ webfront/src/onlinevotingclient.py
@@ -5,7 +5,7 @@ from urllib2 import Request, urlopen, HTTPError
 from werkzeug import SharedDataMiddleware
 import json, urllib
 app_url = ''
-app = Flask(__name__, static_url_path='//NO_IDEA_MATE', static_folder='static')
+app = Flask(__name__, static_url_path='/static', static_folder='static')
 app.secret_key = "SecretAppKeyXxX"
     
 
@@ -27,11 +27,15 @@ def login():
 @app.route(app_url + '/check', methods=['POST', 'GET'])
 def check():
 	values_dict = {}
-	values_dict['login'] = request.form["login"]
-	values_dict['password'] = request.form["password"]
+	values_dict['first_name'] = request.form["first_name"]
+	values_dict['last_name'] = request.form["last_name"]
+        values_dict['fathers_name'] = request.form["fathers_name"]
+        values_dict['mothers_name'] = request.form["mothers_name"]
+        values_dict['id_number'] = request.form["id_number"]
+        values_dict['pesel'] = request.form["pesel"]
 	values = json.dumps(values_dict)
 	headers = {'Content-Type': 'application/json'}
-	requestedData = Request('http://edi.iem.pw.edu.pl/makosak/notes/login', data=values, headers=headers)
+        requestedData = Request('http://localhost:8001/login', data=values, headers=headers)
 	try:
 		response = urlopen(requestedData)
 		response_dict = json.load(response)
@@ -66,10 +70,49 @@ def vote():
         return render_template('/vote.html', first_name = session['first_name'], second_name = session['last_name'])
     else:
         return redirect(app_url + '/login')
-@app.route(app_url + "/candidates")
+@app.route(app_url + "/candidates", methods=['GET'])
 def candidates_list():
-    return render_template('/candidates_list.html')
-
+    candidates_list = []
+    ids = []
+    requestedData = Request('http://localhost:8001/candidates')
+    print("I can't even try. MUTHAFUCKING IDIOTIC")
+    response = urlopen(requestedData)
+    response_dict = json.load(response)
+    print("CAN A MAN GET A CODE?")
+    if response.getcode() == 200:
+        print("CODE WAS 200")
+        candidates_list = response_dict['candidates_list']
+        print(candidates_list)
+        #for value in candidates_list:
+        ids.append(candidates_list['id'])
+    return render_template('/candidates_list.html', ids=ids)
+    #return render_template('/candidates_list.html', ids=ids)
+@app.route(app_url + '/humunculus1')
+def admin1_login():
+    return render_template('/admin_login.html')
+@app.route(app_url + '/humunculus2')
+def admin2_login():
+    return render_template('/admin_login.html')
+@app.route(app_url + '/check_admin')
+def check_admin():
+    values_dict = {}
+    values_dict['id_number'] = request.form["id_number"]
+    values_dict['pesel'] = request.form["pesel"]
+    values_dict['password'] = request.form["password"]
+    values = json.dumps(values_dict)
+    headers = {'Content-Type': 'application/json'}
+    requestedData = Request('http://localhost:8001/login', data=values, headers=headers)
+    try:
+        response = urlopen(requestedData)
+        response_dict = json.load(response)
+        if response.getcode() == 200 and response_dict['message'] == 'OK':
+            session['token'] = response_dict['token']
+            session['user'] = response_dict['user']
+            return redirect(app_url + '/logged')
+        else:
+            return render_template('/login_error.html')
+    except Exception as e:
+        return render_template('/login_error.html')
 @app.errorhandler(404)
 def page_not_found(e):
 	print("Nie znaleziono strony.")
--- webfront/src/static/script.js
+++ webfront/src/static/script.js
@@ -1,104 +1,44 @@
-window.onload = function()
-{
-	var new_note = document.getElementById("new_note");
-	new_note.addEventListener('click', clicked_new_note, false);
+window.onload = function(){
+    var first_name = document.getElementById("first_name");
+    first_name.addEventListener("blur", first_name_check);
+    first_name.addEventListener("keyup", first_name_check);
 
-	var notes_list = document.getElementById("notes_list");
-        notes_list.addEventListener('click', clicked_notes_list, false);
-	
-	var logout = document.getElementById("logout");
-	logout.addEventListener('click', clicked_logout, false);
-	
-	$("#dynamic_panel").on('click',"#note_list #single_note #title",open_note);
-	$("#dynamic_panel").on('click',"#note_list #single_note #delete", delete_note);
-	$("#dynamic_panel").on('click',"#opened_note #delete", delete_note);
-	$("#dynamic_panel").on('click',"#opened_note #edit", edit_note);
-	$("#dynamic_panel").on('click',"#search_options #search", search);
-}
+    var last_name = document.getElementById("last_name");
+    last_name.addEventListener("blur", last_name_check);
+    last_name.addEventListener("keyup", last_name_check);
 
+    var passwd = document.getElementById("password");
+    passwd.addEventListener("blur", password_check);
+    passwd.addEventListener("keyup", password_check);
 
+    var confirm_passwd = document.getElementById("confirm_password");
+    confirm_passwd.addEventListener("blur", confirm_password_check);
+    confirm_passwd.addEventListener("keyup", confirm_password_check);
 
-function search()
-{
-        var type = "x"
-	var data = "x"
-        $.ajax({
-                type: "DELETE",
-                url: "http://edi.iem.pw.edu.pl/makosak/notesclient/" + type.toString() + '/' + data.toString(),
-                dataType: "text",
-                success: function(response) {
-                        var section = document.getElementById("dynamic_panel");
-                        section.innerHTML = response;
-                }
-        });
-}
+    var birth_date = document.getElementById("birth_date");
+    birth_date.addEventListener("blur", birth_date_check);
+    birth_date.addEventListener("keyup", birth_date_check);
 
-	
-function delete_note()
-{
-	var id = $(this).attr("name");
-        $.ajax({
-          	type: "DELETE",
-               	url: "http://edi.iem.pw.edu.pl/makosak/notesclient/delete/" + id.toString(),
-               	dataType: "text",
-               	success: function(response) {
-                       	var section = document.getElementById("dynamic_panel");
-                       	section.innerHTML = response;
-               	}
-        });
-}
-
-function edit_note()
-{
-        var id = $(this).attr("name");
-        $.ajax({
-                type: "PUT",
-                url: "http://edi.iem.pw.edu.pl/makosak/notesclient/edit/" + id.toString(),
-                dataType: "text",
-                success: function(response) {
-                        var section = document.getElementById("dynamic_panel");
-                        section.innerHTML = response;
-                }
-        });
-}
-
-function open_note()
-{
-	var id = $(this).attr("name");
-	$.ajax({
-		type: "GET",
-		url: "http://edi.iem.pw.edu.pl/makosak/notesclient/display/" + id.toString(),
-		dataType: "text",
-		success: function(response) {
-			var section = document.getElementById("dynamic_panel");
-			section.innerHTML = response;
-		}
-	});
-}
-function clicked_new_note()
-{
-	$.ajax({
-           	type: "GET",
-              	url: "http://edi.iem.pw.edu.pl/makosak/notesclient/new_note",
-      		dataType: "text",
-              	success: function (response) {
-			var section = document.getElementById("dynamic_panel");
-			section.innerHTML = response;
-		}
-	});
-}
-function clicked_notes_list()
-{
-        $.ajax({
-                type: "GET",
-                url: "http://edi.iem.pw.edu.pl/makosak/notesclient/get_all_notes",
-                dataType: "text",
-                success: function (response) {
-                        var section = document.getElementById("dynamic_panel");
-                        section.innerHTML = response;
-                }
-        });
-}
+    var login = document.getElementById("login");
+    //login.addEventListener("blur", login_check);
+    //login.addEventListener("keyup", login_check);
+    //
+    //var male = document.getElementById("male");
+    //male.addEventListener("blur", sex_check);
+    //var female = document.getElementById("female");
+    //female.addEventListener("blur", sex_check);
+    //
+    //var id = document.getElementById("id");
+    //id.addEventListener("blur", id_check);
+    //id.addEventListener("keyup", id_check);
+    //
+    //var file = document.getElementById("file");
+    //file.addEventListener("blur", file_check);
+    //
+    //var send = document.getElementById("submit");
+    //send.addEventListener("mouseover", check_all);
+    //send.addEventListener("click", send);
+    //
 function clicked_logout()
 {
 	top.location.href="http://edi.iem.pw.edu.pl/makosak/notesclient/logout"
